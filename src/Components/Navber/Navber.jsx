4aa74179a07a6931634dd38567590487c6fef2b9
Abdullah's PC <abdullahalrahman23@gmail.com>
2023-10-19 21:16:49 +0300

    added navber site logo

--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -1,6 +1,7 @@
 import { Link, NavLink } from "react-router-dom";
 import { useEffect, useState, useContext } from "react";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
+import automotive from '../../assets/cs_meta_automotive.jpg'
 const Navber = () => {
 
     const [carts, setCarts] = useState([]);
@@ -46,7 +47,7 @@ const Navber = () => {
                         }
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+                <a className="btn btn-ghost normal-case text-xl"><img className="w-[80px] rounded-lg" src={automotive} alt="" /></a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
