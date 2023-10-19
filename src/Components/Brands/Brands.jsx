55b6ad472122d10b9076add3aabf3e52c07d66c0
Abdullah's PC <abdullahalrahman23@gmail.com>
2023-10-19 21:18:51 +0300

    fixed brand responsive on home page

--- src/Components/Brands/Brands.jsx
+++ src/Components/Brands/Brands.jsx
@@ -10,7 +10,7 @@ const Brands = () => {
             .then(data => setBrands(data))
     }, [])
     return (
-        <div className="grid grid-cols-3 gap-10 mt-24">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mt-24">
             {
                 brands.map(brand => <Brand key={brand._id} brand={brand}></Brand>)
             }
